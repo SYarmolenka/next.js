6061caaa9ed92a267962e4e7a85d3dc2beb24d5c
Sergey Ermolenko <sergei.ermolenko@upsilonit.com>
2019-02-24 20:12:30 +0300

    fixed

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
-// This file is not going through babel transformation.
-// So, we write it in vanilla JS
-// (But you could use ES2015 features supported by your Node.js version)
-const webpack = require('webpack');
-
-const isProd = (process.env.NODE_ENV || 'production') === 'production'
-
-const assetPrefix = isProd ? '/nextjs' : ''
-
+const debug = process.env.NODE_ENV !== "production";
 module.exports = {
   exportPathMap: function () {
     return {
@@ -14,14 +6,5 @@ module.exports = {
       "/second": { page: "/second" },
     }
   },
-  assetPrefix,
-  webpack: config => {
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        'process.env.ASSET_PREFIX': JSON.stringify(assetPrefix),
-      }),
-    )
-
-    return config
-  },
+  assetPrefix: !debug ? 'https://syarmolenka.github.io/next-gh-page-example/' : '',
 }
