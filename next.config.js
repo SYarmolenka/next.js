f3f3a7fc0609da571d83f432c721dc4b079101cd
Sergey Ermolenko <sergei.ermolenko@upsilonit.com>
2019-02-24 20:36:23 +0300

    Deploy Next.js to gh-pages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
 const debug = process.env.NODE_ENV !== "production";
+
 module.exports = {
   exportPathMap: function () {
     return {
       "/": { page: "/" },
-      "/second": { page: "/second" },
+      "/about": { page: "/about" },
     }
   },
-  assetPrefix: !debug ? 'https://syarmolenka.github.io/next-gh-page-example/' : '',
+  assetPrefix: !debug ? '/next-gh-page-example/' : '',
+  webpack: (config, { dev }) => {
+    config.module.rules = config.module.rules.map(rule => {
+      if(rule.loader === 'babel-loader') {
+        rule.options.cacheDirectory = false
+      }
+      return rule
+    })
+    return config
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,5 @@
-import withStyles from 'react-jss';
 import Link from 'next/link';
 
-const FirstPage = ({ classes }) => (
-    <div className={classes.bgd}>
-      <p>First Page</p>
-      <Link href="/second" prefetch>
-        <a>Second Page</a>
-      </Link>
-    </div>
-);
-
-const styles = {
-  bgd: {
-    background: '#cccccc',
-  },
-};
-
-export default withStyles(styles)(FirstPage);
-
+export default () => (
+  <div>Hello World. <Link href='/about' as={ process.env.BACKEND_URL + '/about'}><a>About</a></Link></div>
+)
