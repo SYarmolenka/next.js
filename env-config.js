b6df90745982d1378442a9921f9eda76d904fb82
Sergey Ermolenko <sergei.ermolenko@upsilonit.com>
2019-02-24 15:42:03 +0300

    fixed

--- env-config.js
+++ env-config.js
@@ -1,5 +1,5 @@
 const prod = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  'process.env.BACKEND_URL': prod ? '/next_js_example' : ''
+  'process.env.BACKEND_URL': prod ? '/Next-gh-page-example' : ''
 }
